946d7d07264959483289e4acc9c21a423f1752a3
Al <pursuance@gmail.com>
2015-03-20 22:41:39 +0100

    slider done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ActiveRecord::Base
   has_many :media
+
+  FIRST_YEAR = -3500
+  LAST_YEAR = Time.now.year
+
+  def inrange
+    ((start_year - Event::FIRST_YEAR) * 100 / (Event::LAST_YEAR - Event::FIRST_YEAR) * 100 ) / 100
+  end
+
+  def self.get_period_percent(range)
+    FIRST_YEAR < 0 ? first_year = - FIRST_YEAR : first_year = FIRST_YEAR
+    total_range = first_year + LAST_YEAR
+    ( range * 100 / total_range * 100 ) / 100
+  end
 end
