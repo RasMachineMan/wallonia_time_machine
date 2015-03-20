3f143ed7ebd3b931534c95f798fcf0ab3325baa8
Maria Lastovka <maria.lastovka@gmail.com>
2015-03-20 17:46:13 +0100

    events view with pictures - still ugly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320142618) do
+ActiveRecord::Schema.define(version: 20150320145742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,16 @@ ActiveRecord::Schema.define(version: 20150320142618) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "media", force: :cascade do |t|
+    t.string   "url"
+    t.string   "type"
+    t.string   "description"
+    t.integer  "event_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "media", ["event_id"], name: "index_media_on_event_id", using: :btree
+
+  add_foreign_key "media", "events"
 end
