6e7551ce721b9507a3a7b0583e1c6f73f340c8df
Tomasz Ras <tomaszras@live.com>
2015-03-20 22:13:25 +0100

    working with maps

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ActiveRecord::Base
   has_many :media
+
+  validates :title, :description, :start_year, :end_year, :latitude, :longitude presence: true
+  # validates :icon_type, :url
+
 end
+
+
+
