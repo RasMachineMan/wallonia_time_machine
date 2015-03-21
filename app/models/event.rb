3bbcf0c66670a791727e19182502544c05233be1
Tomasz Ras <tomaszras@live.com>
2015-03-21 01:16:33 +0100

    map struggle

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   has_many :media
 
-  validates :title, :description, :start_year, :end_year, :latitude, :longitude presence: true
+  # validates :title, :description, :start_year, :end_year, :latitude, :longitude presence: true
   # validates :icon_type, :url
 
 end
